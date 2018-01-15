c288de45115e5fe19a61f90df2e1997605ea1736
Oleg <olegzendel@campus.technion.ac.il>
2018-01-15 18:40:21 +0200

    Parsing and creating a dictionary of similar words

---
+++ main.py
@@ -0,0 +1,68 @@
+# import modules & set up logging
+import logging
+from gensim.models import KeyedVectors
+from queryparsing import QueriesParser
+from ModelInterface import ModelInterface
+from collections import defaultdict
+from difflib import SequenceMatcher
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk import download
+
+# In order to download the stopwords list uncomment the next line
+# download('stopwords')
+
+STOPWORDS = set(stopwords.words('english'))
+QUERYFILE = 'trainqueriesFirst50.xml'
+
+
+def main():
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+    # model_interface = ModelInterface(model)
+    queriesDB = QueriesParser(QUERYFILE)
+
+    # model_interface.test_interface()
+
+    load_model_sym_words(queriesDB)
+    print('finished with the model, printing the synonyms')
+    print('The synonyms dict is {}\n the length is {} (number of keys)'.format(queriesDB.synonyms, len(queriesDB.synonyms)))
+    # print('now starting add similar')
+    # queriesDB.add_similar_words()
+    # print('writing to file')
+    # queriesDB.write_to_file()
+
+
+def load_model_sym_words(queriesDB):
+    # using gzipped/bz2 input works too, no need to unzip:
+    model = KeyedVectors.load_word2vec_format('Word2Vec/google/GoogleNewsnegative300.bin.gz', binary=True)
+
+    for word in queriesDB.get_voc_words():
+        queriesDB.synonyms[word] = get_similar_words(word, model)
+
+
+def get_similar_words(word, model):
+    similar_words = set()
+
+    temp_list = []
+    if model.vocab.get(word) is not None:
+        temp_list += model.wv.similar_by_word(word)
+    if model.vocab.get(word.title()) is not None:
+        temp_list += model.wv.similar_by_word(word.title())
+    if model.vocab.get(word.upper()) is not None:
+        temp_list += model.wv.similar_by_word(word.upper())
+    for symword, score in temp_list:
+        synword = symword.lower().strip('-').strip('#').strip('\'').strip()
+        if synword not in STOPWORDS:
+            if similar(word, synword) < 0.5:
+                if score > 0.3:
+                    similar_words.update(synword.split('_'))
+    return similar_words
+
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+
+if __name__ == '__main__':
+    main()
